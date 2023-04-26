936a30510579e02f0905f3d8cfb4c444ceeee968
arianna <unachoza@gmail.com>
2023-04-26 18:00:52 -0400

    text input form

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Form.styles.css";
 
+interface FormValues {
+  type: string;
+  category: string;
+  amount: number | string;
+  date: string;
+}
 const Form = () => {
-  return <div className="form-content">Form here</div>;
+  const [formValues, setFormValues] = useState<FormValues>({ type: "", category: "", amount: "", date: "" });
+
+  const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    const { name, value } = e.currentTarget;
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div className="form">
+      <div className="title"></div>
+      <div className="form-content">
+        <label>Type</label>
+        <input name="type" type="text" value={formValues.type} placeholder="" onChange={(e) => handleInputChange(e)} />
+        <label>Category</label>
+        <input
+          name="category"
+          type="text"
+          value={formValues.category}
+          placeholder=""
+          onChange={(e) => handleInputChange(e)}
+        />
+        <label>Amount</label>
+        <input
+          name="amount"
+          type="text"
+          value={formValues.amount}
+          placeholder=""
+          onChange={(e) => handleInputChange(e)}
+        />
+        <label>Date</label>
+        <input name="date" type="text" value={formValues.date} placeholder="" onChange={(e) => handleInputChange(e)} />
+      </div>
+    </div>
+  );
 };
 
 export default Form;
